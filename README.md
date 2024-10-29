70f6490795447461631eb3b1a0ad4bb092db39c8
Ran Vaknin <RanVaknin@users.noreply.github.com>
2024-10-29 15:00:52 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# AWS SDK Java v1 Sample Project
+
+This project demonstrates how to use AWS SDK for Java v1 with logging enabled.
+
+## Prerequisites
+
+- Java 17 or later
+- Maven
+- AWS account and credentials configured
+
+## Building the project
+
+To build the project, run:
+
+   ```
+   mvn clean package
+   ```
+
+## Running the application
+
+To run the application, use:
+
+   ```
+   java -jar target/javaSDKProject-1.0-SNAPSHOT-jar-with-dependencies.jar
+   ```
+
+## Project Structure
+
+- `src/main/java/org/example/Main.java`: Main application code
+- `src/main/resources/log4j2.xml`: Logging configuration
+- `pom.xml`: Maven project configuration
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+
+import java.util.List;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.Log4JLogger");
+        listBuckets();
+    }
+
+    public static void listBuckets (){
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion("us-east-1")
+                .build();
+
+        List<Bucket> buckets = s3Client.listBuckets();
+
+        System.out.println("Your Amazon S3 buckets are:");
+        for (Bucket bucket : buckets) {
+            System.out.println("* " + bucket.getName());
+        }
+    }
+
+}
+
